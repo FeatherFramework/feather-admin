67aab49945ed2780a4b937cf809baa953e687c5f
Jake2k4 <119366047+jakeyboi1@users.noreply.github.com>
2023-08-09 15:44:12 -0500

    noClipAdded

--- client/services/boosters.lua
+++ client/services/boosters.lua
@@ -1,5 +1,5 @@
 ----- Variables -----
-local godMode, visible, infStamina = false, false, false
+local godMode, visible, infStamina, noClip = false, false, false, false
 
 ----- Menus -----
 function boostersMenu()
@@ -22,6 +22,12 @@ function boostersMenu()
         table.insert(elements, { label = "Enable Infinite Stamina", value = 'infStamina', desc = "Toggle Infinite Stamina." })
     else
         table.insert(elements, { label = "Disable Infinite Stamina", value = 'infStamina', desc = "Toggle Infinite Stamina." })
+    end
+
+    if not noClip then
+        table.insert(elements, { label = "Enable No Clip", value = 'noClip', desc = "Enable No Clip." })
+    else
+        table.insert(elements, { label = "Disable No Clip", value = 'noClip', desc = "Disable No Clip." })
     end
 
     table.insert(elements, { label = "Heal Self", value = 'heal', desc = "Heal Self." })
@@ -77,6 +83,18 @@ function boostersMenu()
                 end,
                 ['heal'] = function()
                     SetEntityHealth(PlayerPedId(), 100.0)
+                end,
+                ['noClip'] = function()
+                    if not noClip then
+                        noClip = true
+                        VORPMenu.CloseAll()
+                        boostersMenu()
+                        noClipHandler()
+                    else
+                        noClip = false
+                        VORPMenu.CloseAll()
+                        boostersMenu()
+                    end
                 end
             }
 
@@ -96,4 +114,50 @@ function infiniteStamina()
         Wait(5)
         RestorePlayerStamina(PlayerId(), 100.0)
     end
+end
+
+function noClipHandler()
+    local speed = 0.1
+    local playerPed = PlayerPedId()
+    local  PromptGroup = Feather.Prompt:SetupPromptGroup()
+    local speedPrompt = PromptGroup:RegisterPrompt("Change Speed", Feather.Keys.SHIFT, 1, 1, true, 'click')
+    local forwardPrompt = PromptGroup:RegisterPrompt("Forward", Feather.Keys.MOUSE1, 1, 1, true, 'click')
+    local backwardPrompt = PromptGroup:RegisterPrompt("Backward", Feather.Keys.MOUSE2, 1, 1, true, 'click')
+    local upPrompt = PromptGroup:RegisterPrompt("Up", Feather.Keys.CTRL, 1, 1, true, 'click')
+    local downPrompt = PromptGroup:RegisterPrompt("Down", Feather.Keys.LALT, 1, 1, true, 'click')
+
+    local function noClipAction(yoff, zoff, setSpeed) --Credit to vorp admin for this function modified it to make it work
+        local newPos = GetOffsetFromEntityInWorldCoords(playerPed, 0.0, yoff * (setSpeed + 0.3), zoff * (setSpeed + 0.3))
+        SetEntityVelocity(playerPed, 0.0, 0.0, 0.0)
+        SetEntityRotation(playerPed, 0.0, 0.0, 180.0, 0, false)
+        SetEntityHeading(playerPed, GetGameplayCamRelativeHeading())
+        SetEntityCoordsNoOffset(playerPed, newPos.x, newPos.y, newPos.z, false, false, false)
+    end
+
+    --SetEntityVisible(playerPed, false) --Left off for now as first person no clip is iffy still and youll want to know where your char will spawn when exiting noclip
+    SetEntityCollision(playerPed, false, true)
+    FreezeEntityPosition(playerPed, true)
+    SetEveryoneIgnorePlayer(PlayerPedId(), true)
+    while noClip do
+        Wait(5)
+		PromptGroup:ShowGroup("My first prompt group")
+        if speedPrompt:HasCompleted() then
+            speed = speed + 0.1
+            if speed > 2 then speed = 0.1 end
+        end
+
+        if IsControlPressed(0, Feather.Keys.MOUSE1) then
+            noClipAction(-0.2, 0.0, speed)
+        elseif IsControlPressed(0, Feather.Keys.MOUSE2) then
+            noClipAction(0.2, 0.0, speed)
+        elseif IsControlPressed(0, Feather.Keys.CTRL) then
+            noClipAction(0, 1, speed)
+        elseif IsControlPressed(0, Feather.Keys.LALT) then
+            noClipAction(0, -1, speed)
+        end
+    end
+    SetEntityVisible(playerPed, true)
+    SetEntityCollision(playerPed, true, true)
+    FreezeEntityPosition(playerPed, false)
+    SetEveryoneIgnorePlayer(PlayerPedId(), false)
 end
