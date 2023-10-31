c76c42dc4f31f24ea44b6dc670fc81581c530b0a
Jake2k4 <119366047+jakeyboi1@users.noreply.github.com>
2023-10-31 17:40:43 -0500

    newFeatures (#6)

--- server/helpers/trollOptions.lua
+++ server/helpers/trollOptions.lua
@@ -1,19 +1,31 @@
 RegisterServerEvent("feather-admin:TrollCheck", function(event, playerId)
     local options = {
         ["LightningStrike"] = function()
-            TriggerClientEvent("feather-troll:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
         end,
         ["Freeze"] = function()
-            TriggerClientEvent("feather-troll:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
         end,
         ["TeleportToHeaven"] = function()
-            TriggerClientEvent("feather-troll:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
         end,
         ["Kill"] = function()
-            TriggerClientEvent("feather-troll:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
         end,
         ["Cage"] = function()
-            TriggerClientEvent("feather-troll:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+        end,
+        ["makePedGiant"] = function()
+            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+        end,
+        ["forceCinematicCam"] = function()
+            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+        end,
+        ["hostilePedArmy"] = function()
+            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+        end,
+        ['kickFromVehicle'] = function()
+            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
         end
     }
 
