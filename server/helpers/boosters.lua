8f5dacf082c381fb522df3d96937ad9d438f8d87
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-15 03:15:23 -0700

    Update boosters.lua

--- server/helpers/boosters.lua
+++ server/helpers/boosters.lua
@@ -1,26 +1,27 @@
-RegisterServerEvent('feather-admin:PedChangeSender', function(playerId, model)
-    TriggerClientEvent('feather-admin:PedChangeHandler', playerId, model)
-end)
+RegisterNetEvent("feather-admin:BoosterCheck", function(event, playerId)
+    local src = source
+    if not FeatherAdmin.RequireAuthorized(src) then return end
+    local target = FeatherAdmin.ValidTarget(playerId)
+    if target == nil then return end
 
-RegisterServerEvent("feather-admin:BoosterCheck", function(event, playerId)
     local options = {
         ["Invisibility"] = function()
-            TriggerClientEvent('feather-admin:BoosterHandler', playerId, event)
+            TriggerClientEvent('feather-admin:BoosterHandler', target, event)
         end,
         ["Invincibility"] = function()
-            TriggerClientEvent('feather-admin:BoosterHandler', playerId, event)
+            TriggerClientEvent('feather-admin:BoosterHandler', target, event)
         end,
         ["InfStam"] = function()
-            TriggerClientEvent('feather-admin:BoosterHandler', playerId, event)
+            TriggerClientEvent('feather-admin:BoosterHandler', target, event)
         end,
         ["Heal"] = function()
-            TriggerClientEvent('feather-admin:BoosterHandler', playerId, event)
+            TriggerClientEvent('feather-admin:BoosterHandler', target, event)
         end,
         ["disableFOW"] = function()
-            TriggerClientEvent("feather-admin:BoosterHandler", playerId, event)
+            TriggerClientEvent("feather-admin:BoosterHandler", target, event)
         end,
         ["kill"] = function()
-            TriggerClientEvent("feather-admin:BoosterHandler", playerId, event)
+            TriggerClientEvent("feather-admin:BoosterHandler", target, event)
         end
     }
 
