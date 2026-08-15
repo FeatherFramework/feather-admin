6c30b1bd8ff7d41bcd5c383876d9aa4717525d4b
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-15 03:14:26 -0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,19 +1,46 @@
+ClientAllPlayers = {}
+local AdminKeyListener = nil
+
+RegisterNetEvent('feather-admin:ClientAllPlayers', function(players)
+    ClientAllPlayers = players or {}
+end)
+
+function AdminTranslate(key)
+    return Feather.Locale.translate(0, key)
+end
+
 CreateThread(function()
-    TriggerServerEvent('feather-admin:StorePlayersInfo')
-    if Config.Setup.OpenMenu.button then
-        while true do
-            Wait(5)
-            if IsControlJustReleased(0, 0xA5BDCD3C) then
-                if not Inmenu then
-                    Inmenu = true
-                    MainAdminMenu()
-                end
+    if Config.controls.enabled then
+        local menuKey = Config.controls.openMenu
+        AdminKeyListener = Feather.Keys:RegisterListener(menuKey, function ()
+            if not InMenu then
+                TriggerServerEvent('feather-admin:RequestAccess')
             end
-        end
-    else
-        RegisterCommand(Config.Setup.OpenMenu.commandName, function()
-            Inmenu = true
-            MainAdminMenu()
         end)
+    end
+
+    if Config.commands.enabled then
+        local command = Config.commands.openMenu
+        local suggestion = Config.commands.suggestion
+        Feather.Command.Register(command, suggestion, function()
+            if not InMenu then
+                TriggerServerEvent('feather-admin:RequestAccess')
+            end
+        end)
+    end
+end)
+
+RegisterNetEvent('feather-admin:AccessResult', function(authorized)
+    if not authorized or InMenu then return end
+    TriggerServerEvent('feather-admin:RequestPlayerList')
+    MainAdminMenu()
+end)
+
+AddEventHandler('onResourceStop', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then return end
+
+    if AdminKeyListener then
+        AdminKeyListener:RemoveListener()
+        AdminKeyListener = nil
     end
 end)
