9be0f838f36a3360f6f2568ab73acca45e222541
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-15 03:15:13 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,11 @@
-fx_version "adamant"
-games { "rdr3" }
-rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
-lua54 "yes"
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+lua54 'yes'
 
-description 'The official Admin menu for the Feather Framework'
-author 'BCC Scripts'
 name 'feather-admin'
+description 'The official Admin menu for the Feather Framework'
+author 'BCC Team'
 version '0.2.0'
 
 github_version_check 'true'
@@ -14,32 +14,22 @@ github_ui_check 'false'
 github_link 'https://github.com/FeatherFramework/feather-admin'
 
 shared_scripts {
-  "config.lua"
-}
-
-server_scripts {
-  '/server/helpers/allPlayersCatch.lua',
-  '/server/helpers/*.lua',
-  '/server/services/*.lua',
-  "/server/main.lua"
+    'config.lua'
 }
 
 client_scripts {
-  "/client/helpers/functions.lua",
-  '/client/helpers/*.lua',
-  "/client/main.lua",
-  "/client/services/*.lua",
-  "/translations/*.lua"
+    'client/init.lua',
+    'client/main.lua',
+    'translations/*.lua',
+    'client/services/*.lua'
+}
+
+server_scripts {
+    'server/main.lua',
+    'server/helpers/*.lua'
 }
 
 dependencies {
-  'feather-core',
-  'feather-menu'
+    'feather-core',
+    'feather-menu'
 }
-
--- Version Checking --
-name 'feather-admin'
-version '0.1.1'
-github_version_check 'true'
-github_version_type 'release'
-github_link 'https://github.com/FeatherFramework/feather-admin'
