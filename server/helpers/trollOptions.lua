b4db0ef190da5b98b9e32e7110e4c7769a060c05
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-15 03:15:33 -0700

    Update trollOptions.lua

--- server/helpers/trollOptions.lua
+++ server/helpers/trollOptions.lua
@@ -1,37 +1,42 @@
 RegisterServerEvent("feather-admin:TrollCheck", function(event, playerId)
+    local src = source
+    if not FeatherAdmin.RequireAuthorized(src) then return end
+    local target = FeatherAdmin.ValidTarget(playerId)
+    if target == nil then return end
+
     local options = {
         ["LightningStrike"] = function()
-            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", target, event)
         end,
         ["Freeze"] = function()
-            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", target, event)
         end,
         ["TeleportToHeaven"] = function()
-            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", target, event)
         end,
         ["Cage"] = function()
-            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", target, event)
         end,
         ["makePedGiant"] = function()
-            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", target, event)
         end,
         ["forceCinematicCam"] = function()
-            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", target, event)
         end,
         ["hostilePedArmy"] = function()
-            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", target, event)
         end,
         ['kickFromVehicle'] = function()
-            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", target, event)
         end,
         ["handcuffPlayer"] = function()
-            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", target, event)
         end,
         ['hostileBear'] = function()
-            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", target, event)
         end,
         ['lag'] = function()
-            TriggerClientEvent("feather-admin:TrollHandler", playerId, event)
+            TriggerClientEvent("feather-admin:TrollHandler", target, event)
         end
     }
 
