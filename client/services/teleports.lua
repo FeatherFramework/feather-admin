d11195f3e97486de1c61d7488d12558971b86872
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-15 17:18:23 -0700

    Update teleports.lua

--- client/services/teleports.lua
+++ client/services/teleports.lua
@@ -1,68 +1,77 @@
----- Variables -----
-local autoTpm = false
+AdminTeleports = {}
 
----- Local Functions ----
+local state = {
+    autoWaypoint = false,
+    autoWaypointSession = 0,
+    teleporting = false
+}
+
+local function sameWaypoint(first, second)
+    if not first or not second then return false end
+    return Feather.Math.GetDistanceBetween(first, second) < 1.0
+end
+
+local function findGroundZ(x, y)
+    RequestCollisionAtCoord(x, y, 1000.0)
+    for height = 1000, 0, -25 do
+        local found, groundZ = GetGroundZAndNormalFor_3dCoord(x, y, height + 0.0)
+        if found then return groundZ end
+        Wait(0)
+    end
+    return nil
+end
+
 local function teleportToWaypoint()
-    local player = PlayerPedId()
-    local GetGroundZAndNormalFor_3dCoord = GetGroundZAndNormalFor_3dCoord
-    local waypoint = IsWaypointActive()
-    local coords = GetWaypointCoords()
+    if state.teleporting or not IsWaypointActive() then return false end
+    state.teleporting = true
 
-    if waypoint then
-        local x, y, z = coords.x, coords.y, coords.z
-        local groundCheck, ground = nil, nil
-        for height = 1, 1000 do
-            groundCheck, ground = GetGroundZAndNormalFor_3dCoord(x, y, height + 0.0)
-            if groundCheck then
-                break
+    local waypoint = GetWaypointCoords()
+    local groundZ = findGroundZ(waypoint.x, waypoint.y)
+    local destinationZ = groundZ or waypoint.z
+    local ped = PlayerPedId()
+
+    SetEntityCoordsNoOffset(ped, waypoint.x, waypoint.y, destinationZ, false, false, false)
+    if groundZ then
+        Citizen.InvokeNative(0x9587913B9E772D29, ped, false)
+    end
+
+    state.teleporting = false
+    return true
+end
+
+local function runAutoWaypoint(session)
+    local lastWaypoint
+    while state.autoWaypoint and state.autoWaypointSession == session do
+        if IsWaypointActive() then
+            local waypoint = GetWaypointCoords()
+            if not sameWaypoint(waypoint, lastWaypoint) then
+                teleportToWaypoint()
+                lastWaypoint = waypoint
             end
-        end
-
-        if ground and ground > 0.0 then
-            z = ground
-        end
-
-        SetEntityCoords(player, x, y, z)
-        Citizen.InvokeNative(0x9587913B9E772D29, player, 0)
-    end
-end
-
-local function autoTpmFunct()
-    while autoTpm do
-        Wait(5)
-        teleportToWaypoint()
-    end
-end
-
------ Menus -----
-function teleportsMenu()
-    AdminMenu:Close({})
-
-    local teleportPage = AdminMenu:RegisterPage("feather-admin:teleportPage")
-    teleportPage:RegisterElement("header", {
-        value = AdminTranslate("teleportHeader"),
-        slot = 'header',
-        style = {}
-    })
-    teleportPage:RegisterElement("button", {
-        label = AdminTranslate("TPM"),
-        style = {}
-    }, function()
-        teleportToWaypoint()
-    end)
-    teleportPage:RegisterElement("button", {
-        label = AdminTranslate("autoTPM"),
-        style = {}
-    }, function()
-        if not autoTpm then
-            autoTpm = true
-            autoTpmFunct()
         else
-            autoTpm = false
+            lastWaypoint = nil
         end
-    end)
-
-    AdminMenu:Open({
-        startupPage = teleportPage
-    })
+        Wait(250)
+    end
 end
+
+function AdminTeleports.ToWaypoint()
+    CreateThread(teleportToWaypoint)
+end
+
+function AdminTeleports.ToggleAutoWaypoint()
+    state.autoWaypoint = not state.autoWaypoint
+    state.autoWaypointSession = state.autoWaypointSession + 1
+    if state.autoWaypoint then
+        local session = state.autoWaypointSession
+        CreateThread(function()
+            runAutoWaypoint(session)
+        end)
+    end
+end
+
+AddEventHandler('onResourceStop', function(resourceName)
+    if resourceName ~= GetCurrentResourceName() then return end
+    state.autoWaypoint = false
+    state.autoWaypointSession = state.autoWaypointSession + 1
+end)
