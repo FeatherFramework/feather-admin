9f3182a2d39ee50ff97fa5a960491362e209feba
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-15 17:18:55 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,31 +1,35 @@
 Config = {
 
+    -- Keyboard shortcut used to open the admin menu.
     controls = {
-        enabled = true,
-        openMenu = 'PGDN', -- [pagedown]
+        enabled = true,    -- Set to false to disable the shortcut.
+        openMenu = 'PGDN', -- Feather key name; PGDN is Page Down.
     },
 
+    -- Chat command used to open the admin menu.
     commands = {
-        enabled = true,
-        openMenu = 'adminMenu',
-        suggestion = 'Open the Feather Admin menu'
+        enabled = true,                      -- Set to false to disable the command.
+        openMenu = 'adminMenu',              -- Used in chat as /adminMenu.
+        suggestionKey = 'command_suggestion' -- Translation key for chat help text.
     },
 
+    -- Ped models available through the Change Ped Model menu.
     pedChanger = {
-        modelLoadTimeout = 10000,
+        modelLoadTimeout = 10000, -- Maximum loading time in milliseconds.
         categories = {
             {
-                label = 'Human Peds',
+                labelKey = 'human_peds', -- Category translation key.
                 models = {
-                    { label = 'Micah Bell', model = 'cs_micahbell' },
-                    { label = 'Dutch van der Linde', model = 'cs_dutch' }
+                    -- labelKey controls the menu text; model is the game model name.
+                    { labelKey = 'micah_bell',          model = 'cs_micahbell' },
+                    { labelKey = 'dutch_van_der_linde', model = 'cs_dutch' }
                 }
             },
             {
-                label = 'Animal Peds',
+                labelKey = 'animal_peds', -- Category translation key.
                 models = {
-                    { label = 'Alligator', model = 'a_c_alligator_02' },
-                    { label = 'Bear', model = 'a_c_bear_01' }
+                    { labelKey = 'alligator', model = 'a_c_alligator_02' },
+                    { labelKey = 'bear',      model = 'a_c_bear_01' }
                 }
             }
         }
