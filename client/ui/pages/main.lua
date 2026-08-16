8ab28389896649558eb51f4da2788b5804081ac3
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-16 02:01:38 -0700

    Update main.lua

--- client/ui/pages/main.lua
+++ client/ui/pages/main.lua
@@ -4,23 +4,44 @@ function AdminUI.OpenMain()
 
     AdminUI.AddHeader(page, AdminTranslate('admin_header'), AdminTranslate('main_menu'))
 
-    AdminUI.AddButton(page, AdminTranslate('players'), function()
-        TriggerServerEvent('feather-admin:players:request')
-        AdminUI.OpenPlayers()
-    end)
+    if AdminUI.CanUse('players.view') then
+        AdminUI.AddButton(page, AdminTranslate('players'), function()
+            TriggerServerEvent('feather-admin:players:request')
+            AdminUI.OpenPlayers()
+        end)
+    end
 
-    AdminUI.AddButton(page, AdminTranslate('developer_tools'), function()
-        AdminUI.OpenDeveloperTools()
-    end)
+    if AdminUI.CanUseAny('moderation.') then
+        AdminUI.AddButton(page, AdminTranslate('moderation'), function()
+            AdminUI.OpenModeration()
+        end)
+    end
 
-    AdminUI.AddButton(page, AdminTranslate('boosters'), function()
-        AdminUI.SetTarget(GetPlayerServerId(PlayerId()))
-        AdminUI.OpenBoosters(true)
-    end)
+    if AdminUI.CanUseAny('developer.') then
+        AdminUI.AddButton(page, AdminTranslate('developer_tools'), function()
+            AdminUI.OpenDeveloperTools()
+        end)
+    end
 
-    AdminUI.AddButton(page, AdminTranslate('teleport'), function()
-        AdminUI.OpenTeleports()
-    end)
+    if AdminUI.CanUseAny('booster.') or AdminUI.CanUse('ped.change') then
+        AdminUI.AddButton(page, AdminTranslate('boosters'), function()
+            AdminUI.SetTarget(GetPlayerServerId(PlayerId()))
+            AdminUI.OpenBoosters(true)
+        end)
+    end
+
+    if AdminUI.CanUseAny('teleport.') then
+        AdminUI.AddButton(page, AdminTranslate('teleport'), function()
+            AdminUI.OpenTeleports()
+        end)
+    end
+
+    if AdminUI.CanUse('player.spectate') and AdminPlayerManagement.IsSpectating() then
+        local label = AdminTranslate('stop_spectating')
+        AdminUI.AddButton(page, label, function()
+            AdminUI.RunAction(label, AdminPlayerManagement.StopSpectating)
+        end, AdminUI.Styles.danger)
+    end
 
     AdminUI.AddFooter(page)
     AdminUI.AddFooterButton(page, AdminTranslate('close'), function()
