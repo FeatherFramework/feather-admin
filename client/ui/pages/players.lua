f7aea02bba181ab34dd5f9053e014ebfcca59035
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-16 02:00:50 -0700

    Update players.lua

--- client/ui/pages/players.lua
+++ client/ui/pages/players.lua
@@ -1,14 +1,37 @@
 function AdminUI.OpenSelectedPlayer()
+    if not AdminUI.CanUse('players.view') then return end
     local page = AdminUI.RegisterPage('selected_player')
     AdminUI.AddHeader(page, AdminTranslate('admin_header'), AdminTranslate('selected_player_header'))
 
-    AdminUI.AddButton(page, AdminTranslate('boosters'), function()
-        AdminUI.OpenBoosters(false)
-    end)
+    if AdminUI.CanUseAny('player.') then
+        AdminUI.AddButton(page, AdminTranslate('player_management'), function()
+            AdminUI.OpenPlayerManagement()
+        end)
+    end
 
-    AdminUI.AddButton(page, AdminTranslate('trolls'), function()
-        AdminUI.OpenTrolls()
-    end)
+    if AdminUI.CanUseAny('moderation.') then
+        AdminUI.AddButton(page, AdminTranslate('moderation'), function()
+            AdminModeration.SelectOnline(AdminUI.GetTarget())
+        end)
+    end
+
+    if AdminUI.CanUseAny('economy.') or AdminUI.CanUse('character.restore_model') then
+        AdminUI.AddButton(page, AdminTranslate('character_administration'), function()
+            AdminUI.OpenCharacterAdministration()
+        end)
+    end
+
+    if AdminUI.CanUseAny('booster.') or AdminUI.CanUse('ped.change') then
+        AdminUI.AddButton(page, AdminTranslate('boosters'), function()
+            AdminUI.OpenBoosters(false)
+        end)
+    end
+
+    if AdminUI.CanUseAny('troll.') then
+        AdminUI.AddButton(page, AdminTranslate('trolls'), function()
+            AdminUI.OpenTrolls()
+        end)
+    end
 
     AdminUI.AddFooter(page)
     AdminUI.AddFooterButton(page, AdminTranslate('back'), function()
@@ -19,6 +42,7 @@ function AdminUI.OpenSelectedPlayer()
 end
 
 function AdminUI.OpenPlayers()
+    if not AdminUI.CanUse('players.view') then return end
     local page = AdminUI.RegisterPage('players')
     AdminUI.AddHeader(page, AdminTranslate('admin_header'), AdminTranslate('players_header'))
 
