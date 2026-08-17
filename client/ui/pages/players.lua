8055d3f436b63bbca4f7206ea42ee60378468117
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-16 20:25:45 -0700

    Update players.lua

--- client/ui/pages/players.lua
+++ client/ui/pages/players.lua
@@ -3,32 +3,43 @@ function AdminUI.OpenSelectedPlayer()
     local page = AdminUI.RegisterPage('selected_player')
     AdminUI.AddHeader(page, AdminTranslate('admin_header'), AdminTranslate('selected_player_header'))
 
-    if AdminUI.CanUseAny('player.') then
-        AdminUI.AddButton(page, AdminTranslate('player_management'), function()
-            AdminUI.OpenPlayerManagement()
+    if AdminUI.CanUse('player.info') then
+        AdminUI.AddButton(page, AdminTranslate('player_information'), function()
+            AdminPlayerManagement.RequestInfo(AdminUI.GetTarget())
         end)
     end
 
-    if AdminUI.CanUseAny('moderation.') then
+    if AdminUI.CanUse('moderation.view') then
         AdminUI.AddButton(page, AdminTranslate('moderation'), function()
             AdminModeration.SelectOnline(AdminUI.GetTarget())
         end)
     end
 
+    if AdminUI.CanUse('player.go_to') or AdminUI.CanUse('player.bring')
+        or AdminUI.CanUse('player.send_back') or AdminUI.CanUse('player.spectate') then
+        AdminUI.AddButton(page, AdminTranslate('movement'), AdminUI.OpenPlayerManagement)
+    end
+
     if AdminUI.CanUseAny('economy.') or AdminUI.CanUse('character.restore_model') then
-        AdminUI.AddButton(page, AdminTranslate('character_administration'), function()
+        AdminUI.AddButton(page, AdminTranslate('character_and_economy'), function()
             AdminUI.OpenCharacterAdministration()
         end)
     end
 
-    if AdminUI.CanUseAny('booster.') or AdminUI.CanUse('ped.change') then
-        AdminUI.AddButton(page, AdminTranslate('boosters'), function()
+    if AdminUI.CanUsePlayerStatus(false) then
+        AdminUI.AddButton(page, AdminTranslate('player_status'), function()
             AdminUI.OpenBoosters(false)
         end)
     end
 
-    if AdminUI.CanUseAny('troll.') then
-        AdminUI.AddButton(page, AdminTranslate('trolls'), function()
+    if AdminUI.CanUse('ped.change') or AdminUI.CanUse('troll.make_ped_giant') then
+        AdminUI.AddButton(page, AdminTranslate('appearance'), function()
+            AdminUI.OpenAppearance('selected_player')
+        end)
+    end
+
+    if AdminUI.CanUseSpecialEffects() then
+        AdminUI.AddButton(page, AdminTranslate('special_effects'), function()
             AdminUI.OpenTrolls()
         end)
     end
