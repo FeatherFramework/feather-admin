d25c71013775865cc38e86a4a0c731e8cacce7d7
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-16 20:25:24 -0700

    Create appearance.lua

---
+++ client/ui/pages/appearance.lua
@@ -0,0 +1,30 @@
+function AdminUI.OpenAppearance(parentPage)
+    if parentPage then AdminUI.appearanceParent = parentPage end
+    if not AdminUI.CanUse('ped.change') and not AdminUI.CanUse('troll.make_ped_giant') then return end
+
+    local page = AdminUI.RegisterPage('appearance')
+    AdminUI.AddHeader(page, AdminTranslate('admin_header'), AdminTranslate('appearance_header'))
+
+    if AdminUI.CanUse('ped.change') then
+        AdminUI.AddButton(page, AdminTranslate('change_ped_model'), AdminUI.OpenPedCategories)
+    end
+
+    if AdminUI.CanUse('troll.make_ped_giant') then
+        local label = AdminTranslate('toggle_giant_ped')
+        AdminUI.AddButton(page, AdminUI.GetToggleLabel(label, 'make_ped_giant'), function(_, element)
+            AdminUI.RunToggleAction(label, 'make_ped_giant', element, function()
+                AdminTrolls.Request('make_ped_giant', AdminUI.GetTarget())
+            end)
+        end)
+    end
+
+    AdminUI.AddFooter(page)
+    AdminUI.AddFooterButton(page, AdminTranslate('back'), function()
+        if AdminUI.appearanceParent == 'self_tools' then
+            AdminUI.OpenSelfTools()
+        else
+            AdminUI.OpenSelectedPlayer()
+        end
+    end)
+    AdminUI.OpenPage('appearance')
+end
