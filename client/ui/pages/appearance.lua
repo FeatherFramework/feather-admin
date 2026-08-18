60227eadad07fd189e2895854ff79fa61e5dc867
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-18 03:44:24 -0700

    Update appearance.lua

--- client/ui/pages/appearance.lua
+++ client/ui/pages/appearance.lua
@@ -1,15 +1,15 @@
 function AdminUI.OpenAppearance(parentPage)
     if parentPage then AdminUI.appearanceParent = parentPage end
-    if not AdminUI.CanUse('ped.change') and not AdminUI.CanUse('troll.make_ped_giant') then return end
+    if not AdminUI.CanUseOnTarget('ped.change') and not AdminUI.CanUseOnTarget('troll.make_ped_giant') then return end
 
     local page = AdminUI.RegisterPage('appearance')
     AdminUI.AddHeader(page, AdminTranslate('admin_header'), AdminTranslate('appearance_header'))
 
-    if AdminUI.CanUse('ped.change') then
+    if AdminUI.CanUseOnTarget('ped.change') then
         AdminUI.AddButton(page, AdminTranslate('change_ped_model'), AdminUI.OpenPedCategories)
     end
 
-    if AdminUI.CanUse('troll.make_ped_giant') then
+    if AdminUI.CanUseOnTarget('troll.make_ped_giant') then
         local label = AdminTranslate('toggle_giant_ped')
         AdminUI.AddButton(page, AdminUI.GetToggleLabel(label, 'make_ped_giant'), function(_, element)
             AdminUI.RunToggleAction(label, 'make_ped_giant', element, function()
