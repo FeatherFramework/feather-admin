68da9de08f7adfaaae7acd6d66d6cb6bcc49ed4f
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-21 00:36:13 -0700

    Update init.lua

--- client/core/init.lua
+++ client/core/init.lua
@@ -4,14 +4,29 @@ FeatherMenu = exports['feather-menu'].initiate()
 InMenu = false
 ClientAllPlayers = {}
 AdminPermissions = {}
+AdminPlayerDirectory = {
+    query = '',
+    results = {},
+    selected = nil,
+    roles = {},
+    roleFilterId = nil
+}
 
 AdminStaff = {
     roles = {},
     players = {},
     results = {},
     selectedTarget = nil,
+    pendingTarget = nil,
     selectedRole = nil,
+    roleFilterId = nil,
+    reason = '',
     searchQuery = nil,
+    searchPage = 1,
+    searchHasNext = false,
+    history = {},
+    historyPage = 1,
+    historyHasNext = false,
     origin = 'online'
 }
 
