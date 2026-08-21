605b43be007211497eb971214615763be65384c6
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-21 13:52:32 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ lua54 'yes'
 name 'feather-admin'
 description 'The official Admin menu for the Feather Framework'
 author 'BCC Scripts'
-version '0.2.2'
+version '0.3.0'
 
 github_version_check 'true'
 github_version_type 'release'
