9c42691bffea6884fd9c4d33d1f2f2a84b4457ec
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-27 02:17:39 -0700

    Update init.lua

--- client/core/init.lua
+++ client/core/init.lua
@@ -1,4 +1,37 @@
-Feather = exports['feather-core'].initiate()
+Feather = {}
+Feather.RPC = {
+    Notify = function(name, params, source)
+        return exports['feather-core']:NotifyRPC(name, params, source)
+    end
+}
+Feather.Locale = {
+    register = function(locale, translations)
+        return exports['feather-core']:RegisterLocale(locale, translations)
+    end,
+    translate = function(source, key, ...)
+        local result = exports['feather-core']:TranslateLocale(source, key, ...)
+        return type(result) == 'table' and result.ok == true and result.value
+            or ('Translation [%s] is unavailable'):format(tostring(key))
+    end
+}
+
+local function ShowNotification(request)
+    local result = exports['feather-core']:ShowNotification(request)
+    if type(result) ~= 'table' or result.ok ~= true then
+        print(('[feather-admin] client notification failed code=%s'):format(
+            tostring(type(result) == 'table' and result.code or 'invalid_result')))
+    end
+    return result
+end
+
+Feather.Notify = {
+    RightNotify = function(message, duration)
+        return ShowNotification({ style = 'right', message = message, duration = duration })
+    end,
+    TopBanner = function(title, message, duration)
+        return ShowNotification({ style = 'top_banner', title = title, message = message, duration = duration })
+    end
+}
 FeatherMenu = exports['feather-menu'].initiate()
 
 InMenu = false
