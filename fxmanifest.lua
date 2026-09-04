4fa0488580d72bfbf70b863721ece44a1b28b653
iseeyoucopy <2729972+iseeyoucopy@users.noreply.github.com>
2026-09-04 14:40:13 +0300

    Add Romanian translation (#25)
    * Add Romanian translation
    
    Adds Romanian language support to Feather Admin.
    
    * Update fxmanifest.lua
    
    Adds Romanian language support and corrects the version bump for the Feather Menu v2 migration.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ lua54 'yes'
 name 'feather-admin'
 description 'The official Admin menu for the Feather Framework'
 author 'BCC Scripts'
-version '0.7.2'
+version '0.8.0'
 
 ui_page 'web/noclip.html'
 
